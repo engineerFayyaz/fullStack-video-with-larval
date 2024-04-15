99bb2ae882acdfb4eaf3d2ba4fbf3bb40f28fbb7
codesinc-repo <codes.inc.com@gmail.com>
2024-04-15 11:59:32 +0500

    timer updated

--- src/components/UpcomingMoviesTimer.js/index.jsx
+++ src/components/UpcomingMoviesTimer.js/index.jsx
@@ -16,42 +16,35 @@ const UpcomingEventTimerBar = () => {
         if (response.status === 200) {
           const responseData = response.data;
 
-          // Check if responseData and responseData.data are present and are arrays
           if (responseData && responseData.data && Array.isArray(responseData.data) && responseData.data.length > 0) {
-            const upcomingTimesData = responseData.data.map((movie) => movie.timeonly);
+            const upcomingTimesData = responseData.data.map((movie) => ({
+              timeonly: movie.timeonly,
+              date: movie.date
+            }));
             setUpcomingTimes(upcomingTimesData);
           } else {
-            // console.error("No upcoming times in API response. Generating random schedule.");
             generateRandomSchedule();
           }
         } else {
-          // console.error("Error fetching upcoming times. Status:", response.status);
           generateRandomSchedule();
         }
       } catch (error) {
-        // console.error("Error fetching upcoming times:", error.message);
         generateRandomSchedule();
       }
     };
 
     const generateRandomSchedule = () => {
       const randomTimes = ["8:00PM", "9:30AM", "12:00PM", "1:00AM", "2:45AM"];
-      
-      // Get the current date and time
       const currentDate = new Date();
-      
-      // Generate random dates and times for the next 30 days
       const randomSchedule = Array.from({ length: 10 }, (_, index) => {
         const randomDate = new Date();
-        randomDate.setDate(currentDate.getDate() + index + 1); // Increment the date
+        randomDate.setDate(currentDate.getDate() + index + 1);
         const formattedDate = `${randomDate.getMonth() + 1}-${randomDate.getDate()}-${randomDate.getFullYear()}`;
-        
         return {
           timeonly: randomTimes[index % randomTimes.length],
           date: formattedDate,
         };
       });
-
       setUpcomingTimes(randomSchedule);
     };
 
@@ -60,9 +53,9 @@ const UpcomingEventTimerBar = () => {
 
   return (
     <div className="row" style={{ margin: "20px 0px 0px 40px " }}>
-      <h4 style={{ color: "white",fontWeight:"700",fontSize:"20px", marginLeft:"-30px",marginTop:"155px",marginBottom:"15px" }}>Upcoming</h4>
+      <h4 style={{ color: "white", fontWeight: "700", fontSize: "20px", marginLeft: "-30px", marginTop: "155px", marginBottom: "15px" }}>Upcoming</h4>
       <div className="step-container upcomong-time">
-        {upcomingTimes.map((upcomingTime, index) => (
+        {upcomingTimes && upcomingTimes.length > 0 && upcomingTimes.map((upcomingTime, index) => (
           <div key={index} className="step" title={`Date: ${upcomingTime.date}`}>
             {`${upcomingTime.timeonly}`}
           </div>
